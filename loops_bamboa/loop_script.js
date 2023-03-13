a23f7b6d7f2902f88aec7a6b7bf0be0235bbfff3
alexiaannika07 <127203399+alexiaannika07@users.noreply.github.com>
2023-03-13 11:10:10 +0800

    Add files via upload

---
+++ loops_bamboa/loop_script.js
@@ -0,0 +1,19 @@
+var x=0;
+
+while(x<60){
+	x=x+2;
+	document.writeln(x);
+}
+
+//2. 
+var y=0
+do{
+	y=y+3; 
+	document.writeln(y);
+}while(y<60)
+
+//3. 
+var a=0;
+for (a=5;a<51;a=a+5){
+	document.writeln(a);
+}
